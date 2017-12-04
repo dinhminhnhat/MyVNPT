95a40165fc5c057aa16857e26f695af0721e45dd
Nhat <nhat.dinhminh@uni.dntu.edu.vn>
2017-12-04 13:56:38 +0700

    reamke title

--- app/src/main/java/vn/com/vnpt/myvnpt/MainActivity.java
+++ app/src/main/java/vn/com/vnpt/myvnpt/MainActivity.java
@@ -2,12 +2,15 @@ package vn.com.vnpt.myvnpt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
     }
 }
