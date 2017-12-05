a44a0e91219c5950793dc073850813ec9c6c8e32
Nhat <nhat.dinhminh@uni.dntu.edu.vn>
2017-12-05 15:05:27 +0700

    reamke 4 title

--- app/src/main/java/vn/com/vnpt/myvnpt/MainActivity.java
+++ app/src/main/java/vn/com/vnpt/myvnpt/MainActivity.java
@@ -19,11 +19,26 @@ ImageView avatar;
 ListView lvDanhSack;
 EditText editTim;
 Button btn_Tim;
+    public static int[] imgAvatar = {R.mipmap.gn,R.mipmap.birthday,R.mipmap.valentine};
+    //Nội dung của từng Item trong ListView
+    public static String[] so = {"0909090990", "0123456789",
+            "Valentine"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        Anhxa();
+        lvDanhSack = (ListView) findViewById(R.id.lvDanhSack);
+        //Khởi tạo đối tượng adapter và gán Data source --> gán Adapter vào Lisview
+        //Ở bước này mình đã làm tắt đi 1 bước, chính xác là
+//        //CustomAdapter adapter=new CustomAdapter(CustomListVIewActivity.this, tvNoiDung, imgAvatar);
+//        //lvCustomListView.setAdapter(adapter);
+        lvDanhSack.setAdapter(new customlistviewAdapter(MainActivity.this,so,imgAvatar));
+    }
+
+    public void Anhxa(){
         sosim = (TextView)findViewById(R.id.tv_sosim);
         thongtin = ( TextView) findViewById(R.id.tv_thongtin);
         trangthai = (TextView) findViewById(R.id.tv_trangthaisim);
